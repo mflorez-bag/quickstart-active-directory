2ae5930439f5b5ac2e1e91e5eb253c9d135b0486
mflorez-bag <mflorez@baggenstos.ch>
2024-06-06 14:14:02 +0200

    copied from quickstart microsoft, updated to fix error, eventually

---
+++ active-directory-new-domain-ha-2-dc/DSC/CreateADPDC/CreateADPDC.ps1
@@ -0,0 +1,114 @@
+﻿configuration CreateADPDC
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    $Interface = Get-NetAdapter | Where-Object { $_.Name -Like "Ethernet*" } | Select-Object -First 1
+    if (-not $Interface) {
+        throw "No Ethernet adapter found."
+    }
+    $InterfaceAlias = $Interface.Name
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
+            Name = "DNS"
+        }
+
+        Script EnableDNSDiags
+        {
+            SetScript = {
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Message "Enabling DNS client diagnostics"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = '127.0.0.1'
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xWaitForDisk Disk1
+        {
+            DiskNumber = 1
+            RetryIntervalSec = $RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber = 1
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk1"
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+
+        xADDomain FirstDS
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+            DependsOn = @("[WindowsFeature]ADDSInstall", "[xDisk]ADDataDisk")
+        }
+
+        xPendingReboot RebootAfterPromotion {
+            Name = "RebootAfterPromotion"
+            DependsOn = "[xADDomain]FirstDS"
+        }
+    }
+}
